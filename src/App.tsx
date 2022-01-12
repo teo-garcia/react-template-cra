3ca525ad5a53a7804ddb5a739e474ab6b9b9345c
teo-garcia <juanmateogarcia96@gmail.com>
2022-01-11 19:31:21 -0500

    feat: Add react router and react helmet

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import './App.scss';
 import { Global } from './components/Global/Global';
 import { Layout } from './components/Layout/Layout';
-import { Typography } from './components/Typography/Typography';
+import { Router } from './components/Router/Router';
 
 function App() {
   return (
     <Global>
       <Layout>
-        <Typography weight="bold" color="secondary" className="title">
-          Hello World
-        </Typography>
+        <Router />
       </Layout>
     </Global>
   );
---
+++ src/components/Router/Router.tsx
@@ -0,0 +1,12 @@
+import { Route, Routes } from 'react-router-dom';
+import { Home } from '../../pages';
+
+function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
+}
+
+export { Router };
